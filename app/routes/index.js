fb44307bbecc01a2357d6c42a52826a7556bd056
Jamie Rezendes <jamierezendes@gmail.com>
2016-06-14 11:44:47 -0700

    updated

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,10 @@ function init(app,passport){
 		res.render(path + '/views/index.jade') , {user : req.user};
 	});
 
+	app.get('/google52bb4afbc6fc2d41.html', function(req,res){
+		res.render(path + '/views/google52bb4afbc6fc2d41.html');
+	});
+
 	app.get('/auth/google/get', passport.authenticate('google', { scope
 		 : ['https://www.googleapis.com/auth/plus.me' ,
 		  'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile']}
