f152af4d78b42d89ccb775d27bd9e94527f6d852
Jamie Rezendes <jamierezendes@gmail.com>
2016-06-16 00:59:17 -0700

    updated Google CB Url

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
   'googleAuth' : {
         'clientID'      : '386322467441-hs4h8go2ldtv21n5hl710o84mgqsticd.apps.googleusercontent.com',
         'clientSecret'  : 'nbPneaz4pxecMokDw8zHe0gz',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : 'https://fcc-my-night.herokuapp.com/auth/google'
   }
 }
