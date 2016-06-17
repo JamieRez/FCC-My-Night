94650ffdec55f5722cd5e384cbf74878eef37953
Jamie Rezendes <jamierezendes@gmail.com>
2016-06-17 09:54:47 -0700

    updated google callback

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
   'googleAuth' : {
         'clientID'      : '386322467441-hs4h8go2ldtv21n5hl710o84mgqsticd.apps.googleusercontent.com',
         'clientSecret'  : 'nbPneaz4pxecMokDw8zHe0gz',
-        'callbackURL'   : 'https://fcc-my-night.herokuapp.com/auth/google'
+        'callbackURL'   : 'https://fcc-my-night.herokuapp.com/auth/google/callback'
   }
 }
